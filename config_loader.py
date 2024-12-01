357d610a66c29d84928ab51442a265031b419c75
Eli Young <eliyoung4now@gmail.com>
2024-11-30 23:27:03 -0500

    upl

--- config_loader.py
+++ config_loader.py
@@ -8,17 +8,21 @@ load_dotenv()
 def load_config():
     with open('config.yaml', 'r') as file:
         # Load the YAML template
-        config = yaml.safe_load(file)
+        template = file.read()
         
-        # Replace environment variables
-        config['username'] = os.getenv('USERNAME')
-        config['password'] = os.getenv('PASSWORD')
-        config['phone_number'] = os.getenv('PHONE_NUMBER')
+        # Manually substitute environment variables
+        template = template.replace('${USERNAME}', os.getenv('USERNAME', ''))
+        template = template.replace('${PASSWORD}', os.getenv('PASSWORD', ''))
+        template = template.replace('${PHONE_NUMBER}', os.getenv('PHONE_NUMBER', ''))
+        template = template.replace('${RESUME_PATH}', os.getenv('RESUME_PATH', ''))
+        template = template.replace('${CV_PATH}', os.getenv('CV_PATH', ''))
         
-        # Update file paths
-        config['uploads']['Resume'] = os.getenv('RESUME_PATH')
-        config['uploads']['Cover Letter'] = os.getenv('CV_PATH')
-        config['resume_path'] = os.getenv('RESUME_PATH')
+        # Load YAML after substitution
+        config = yaml.safe_load(template)
+        
+        # Clean up positions (remove extra quotes and split properly)
+        if isinstance(config['positions'][0], str):
+            config['positions'] = [pos.strip() for pos in config['positions'][0].split(',')]
         
         return config
 
